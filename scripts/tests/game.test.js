0d9c852c51c8e574ce53ae6071a34de65cb1bebf
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-01-07 20:45:40 +0100

    Create test for show turns and make function work

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-const {game, newGame, showScore, addTurn, lightsOn} = require("../game");
+const {game, newGame, showScore, addTurn, lightsOn, showTurns} = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -88,5 +88,11 @@ describe("game play works correct", () => {
         let button = document.getElementById(game.currentGame[0]);
         lightsOn(game.currentGame[0]);
         expect(button.classList).toContain("light");
+    })
+
+    test("showTurns should update game.turnNumber", () => {
+        game.turnNumber = 42;
+        showTurns();
+        expect(game.turnNumber).toBe(0);
     })
 }); 
