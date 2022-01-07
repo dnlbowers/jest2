3b7b30298319beb2dec03ef676d01054810e58c4
David Bowers <82392777+dnlbowers@users.noreply.github.com>
2022-01-07 23:53:42 +0100

    create player turn with test

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,7 +1,7 @@
 /**
  * @jest-environment jsdom
  */
-const {game, newGame, showScore, addTurn, lightsOn, showTurns} = require("../game");
+const {game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn} = require("../game");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -110,5 +110,11 @@ describe("game play works correct", () => {
         game.turnNumber = 42;
         showTurns();
         expect(game.turnNumber).toBe(0);
+    })
+
+    test("should increment the score if the turn is correct", () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
     })
 }); 
